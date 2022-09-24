b7ed94b019bd9786d6f40d2e95016b235fed8877
Vladislav_qa <1savenkov.vladislav@gmail.com>
2022-09-24 15:08:30 +0800

    added_an_integrated_test

--- cypress/integration/1-My-tests/staya3.js
+++ cypress/integration/1-My-tests/staya3.js
@@ -3,8 +3,8 @@ describe('[+] Вход на сайт с валидными данными', func
     it('Ввод логина и пароля, клик по кнопке Войти', function () {
        cy.visit('https://staya.dog/');
        cy.get('.header-bottom__right--link').click()
-       cy.get('.auth-form > form > [type="email"]').type('1savenkov.vladislav@gmail.com');
-       cy.get('.auth-form > form > [type="password"]').type('DlyaAvtoTests99');
+       cy.get('.auth-form > form > [type="email"]').type('Ввести_валидный_логин');
+       cy.get('.auth-form > form > [type="password"]').type('Ввести_валидный_пароль');
        cy.get('.auth-form__submit > .s-button__content').click();
        cy.contains('Мои заказы'); // Проверяем, что авторизация прошла успешно
        cy.get('button.profile__nav-link').click(); // Выходим из учетной записи для проверки следующего кейса
@@ -17,8 +17,8 @@ describe('[-} Вход на сайт с неверным паролем', functi
 
     it('Ввод логина и пароля, клик по кнопке Войти', function () {
        cy.get('.header-bottom__right--link').click()
-       cy.get('.auth-form > form > [type="email"]').type('1savenkov.vladislav@gmail.com');
-       cy.get('.auth-form > form > [type="password"]').type('DlyaAvtoTests');
+       cy.get('.auth-form > form > [type="email"]').type('Ввести_валидный_логин');
+       cy.get('.auth-form > form > [type="password"]').type('Ввести_невалидный_пароль');
        cy.get('.auth-form__submit > .s-button__content').click();
        cy.contains('Невозможно войти с предоставленными учетными данными.'); // Проверяем, что авторизация не прошла
        })
